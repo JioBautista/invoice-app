031d313246961bbd328c9316455a42b1952cb2f9
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-03-18 09:11:14 -0400

    Updates

--- frontend/app/src/components/InvoiceList.jsx
+++ frontend/app/src/components/InvoiceList.jsx
@@ -1,52 +1,73 @@
 import * as React from "react";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
 import { Typography } from "@mui/material";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 export default function InvoiceList({ data }) {
   return (
     <>
-      {data.map((items) => (
-        <>
-          <Paper sx={{ mb: 3, padding: 3 }} elevation={3}>
-            <Grid
-              container
-              spacing={0.5}
-              justifyContent={"space-between"}
-              alignItems={"center"}
-              sx={{ cursor: "pointer" }}
+      {data ? (
+        data.map((items) => (
+          <React.Fragment key={items.id}>
+            <Paper sx={{ mb: 3, padding: 3 }} elevation={3}>
+              <Grid
+                container
+                spacing={0.5}
+                justifyContent={"space-between"}
+                alignItems={"center"}
+                sx={{ cursor: "pointer" }}
+              >
+                <Grid item xs={3} sm={2}>
+                  <Typography sx={{ fontWeight: "bold" }}>
+                    #{items.invoice_num}
+                  </Typography>
+                </Grid>
+                <Grid item xs={9} sm={2}>
+                  <Typography sx={{ color: "#858BB2" }} variant="body1">
+                    {items.client_name}
+                  </Typography>
+                </Grid>
+                <Grid item xs={"auto"} sm={3}>
+                  <Typography sx={{ color: "#7E88C3" }} variant="body1">
+                    Due {items.payment_due}
+                  </Typography>
+                </Grid>
+                <Grid item xs={"auto"} sm={2}>
+                  <Typography sx={{ fontWeight: "bold" }} variant="body1">
+                    {items.total}
+                  </Typography>
+                </Grid>
+                <Grid item xs={"auto"} sm={1}>
+                  <Typography variant="button">{items.status}</Typography>
+                </Grid>
+                <Grid item xs={"auto"}>
+                  <ArrowForwardIosIcon color="primary" fontSize="small" />
+                </Grid>
+              </Grid>
+            </Paper>
+          </React.Fragment>
+        ))
+      ) : (
+        <Box>
+          <Stack spacing={1} alignItems={"center"} sx={{ textAlign: "center" }}>
+            <img src="/assets/illustration-empty.svg" />
+            <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+              There is nothing here
+            </Typography>
+            <Typography
+              sx={{ width: "29ch", color: "#888EB0" }}
+              variant="subtitle1"
             >
-              <Grid item xs={3} sm={2}>
-                <Typography sx={{ fontWeight: "bold" }}>
-                  #{items.invoice_num}
-                </Typography>
-              </Grid>
-              <Grid item xs={9} sm={2}>
-                <Typography sx={{ color: "#858BB2" }} variant="body1">
-                  {items.client_name}
-                </Typography>
-              </Grid>
-              <Grid item xs={"auto"} sm={3}>
-                <Typography sx={{ color: "#7E88C3" }} variant="body1">
-                  Due {items.payment_due}
-                </Typography>
-              </Grid>
-              <Grid item xs={"auto"} sm={2}>
-                <Typography sx={{ fontWeight: "bold" }} variant="body1">
-                  {items.total}
-                </Typography>
-              </Grid>
-              <Grid item xs={"auto"} sm={1}>
-                <Typography variant="button">{items.status}</Typography>
-              </Grid>
-              <Grid item xs={"auto"}>
-                <ArrowForwardIosIcon color="primary" fontSize="small" />
-              </Grid>
-            </Grid>
-          </Paper>
-        </>
-      ))}
+              Create an invoice by clicking the{" "}
+              <span style={{ fontWeight: "bold" }}>New</span> button and get
+              started
+            </Typography>
+          </Stack>
+        </Box>
+      )}
     </>
   );
 }
--- frontend/app/src/components/Menu.jsx
+++ frontend/app/src/components/Menu.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Filter from "./Filter";
 import Box from "@mui/material/Box";
 import { Button } from "@mui/material";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
@@ -7,6 +6,10 @@ import IconButton from "@mui/material/IconButton";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
@@ -14,6 +17,11 @@ import "@fontsource/roboto/700.css";
 
 function Menu({ data }) {
   const mobile = useMediaQuery("(max-width:500px)");
+  const [status, setStatus] = React.useState("");
+
+  const handleChange = (event) => {
+    setStatus(event.target.value);
+  };
 
   return (
     <Box sx={{ mb: 10 }}>
@@ -24,14 +32,34 @@ function Menu({ data }) {
         spacing={2}
       >
         <Box sx={{ flexGrow: 1 }}>
-          <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+          <Typography
+            variant={mobile ? "h6" : "h4"}
+            sx={{ fontWeight: "bold" }}
+          >
             Invoices
           </Typography>
-          <Typography variant="subtitle1">
-            {data ? `Total number of invoices: ${data.length}` : "No invoices"}
+          <Typography variant="body1">
+            {data ? `Total number of invoices: ${data.length}` : `No invoices`}
           </Typography>
         </Box>
-        <Filter />
+
+        <Box sx={{ minWidth: 120 }}>
+          <FormControl fullWidth variant="standard" size="small">
+            <InputLabel id="demo-simple-select-label">Filter</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={status}
+              label="Filter"
+              onChange={handleChange}
+            >
+              <MenuItem value={"paid"}>Paid</MenuItem>
+              <MenuItem value={"pending"}>Pending</MenuItem>
+              <MenuItem value={"draft"}>Draft</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+
         {mobile ? (
           <IconButton color="primary">
             <AddCircleIcon fontSize="large" />
