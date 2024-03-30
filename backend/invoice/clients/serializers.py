5c9fa92be05960d85c1d296c88d9d7f2489d8b8f
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-03-30 14:20:51 -0400

    Updated server

--- backend/invoice/clients/serializers.py
+++ backend/invoice/clients/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from clients.models import ClientInfo, ClientAddress
+from clients.models import ClientInfo, ClientAddress, SenderAddress, ClientItems
 from django.contrib.auth.models import User
 
 
@@ -10,8 +10,24 @@ class ClientAddressSerializers(serializers.ModelSerializer):
         fields = ["street", "city", "postCode", "country"]
 
 
+class SenderAddressSerializers(serializers.ModelSerializer):
+
+    class Meta:
+        model = SenderAddress
+        fields = ["street", "city", "postCode", "country"]
+
+
+class ClientItemsSerializers(serializers.ModelSerializer):
+
+    class Meta:
+        model = ClientItems
+        fields = ["name", "price", "total", "quantity"]
+
+
 class ClientsSerializers(serializers.ModelSerializer):
     client_address = ClientAddressSerializers()
+    sender_address = SenderAddressSerializers()
+    items = ClientItemsSerializers(many=True)
 
     class Meta:
         model = ClientInfo
@@ -24,19 +40,38 @@ class ClientsSerializers(serializers.ModelSerializer):
             "payment_terms",
             "status",
             "total",
-            "sender_address",
             "client_name",
             "client_email",
-            "items",
+            "sender_address",
             "client_address",
+            "items",
         ]
         depth = 1
         owner = serializers.ReadOnlyField(source="owner.username")
 
     def create(self, validated_data):
-        address = validated_data.pop("client_address")
-        client = ClientInfo.objects.create(**validated_data)
-        ClientAddress.objects.create(**address)
+        # EXTRACT ITEM DATA
+        items_data = validated_data.pop("items")
+
+        # EXTRACT SENDERS ADDRESS DATA AND INSERT IN SENDER ADDRESS MODEL
+        sender_data = validated_data.pop("sender_address")
+        sender_address = SenderAddress.objects.create(**sender_data)
+
+        # EXTRACT CLIENT ADDRESS DATA AND INSERT IN CLIENT ADDRESS MODEL
+        address_data = validated_data.pop("client_address")
+        client_address = ClientAddress.objects.create(**address_data)
+
+        # CLIENT INFO MODELS W/ CLIENT ADDRESS, SENDER ADDRESS AND ITEMS FIELD
+        client = ClientInfo.objects.create(
+            client_address=client_address,
+            sender_address=sender_address,
+            **validated_data
+        )
+
+        # INSERT ITEMS DATA IN CLIENT ITEMS MODEL
+        for data in items_data:
+            item = ClientItems.objects.create(**data)
+            client.items.add(item)
         return client
 
 
