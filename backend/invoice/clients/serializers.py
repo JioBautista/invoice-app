3505ae1f352ad6f1b51eb00fa75361fdb167d156
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-03-30 09:35:49 -0400

    Updates

--- backend/invoice/clients/serializers.py
+++ backend/invoice/clients/serializers.py
@@ -7,25 +7,37 @@ class ClientAddressSerializers(serializers.ModelSerializer):
 
     class Meta:
         model = ClientAddress
-        fields = "__all__"
+        fields = ["street", "city", "postCode", "country"]
 
 
 class ClientsSerializers(serializers.ModelSerializer):
-    client_address = ClientAddressSerializers(read_only=True)
+    client_address = ClientAddressSerializers()
 
     class Meta:
         model = ClientInfo
-        exclude = ["owner"]
+        fields = [
+            "id",
+            "invoice_num",
+            "created_at",
+            "payment_due",
+            "description",
+            "payment_terms",
+            "status",
+            "total",
+            "sender_address",
+            "client_name",
+            "client_email",
+            "items",
+            "client_address",
+        ]
         depth = 1
         owner = serializers.ReadOnlyField(source="owner.username")
 
     def create(self, validated_data):
-        print(validated_data)
-        client_address_data = validated_data.pop("client_address")
-        client_info = ClientInfo.objects.create(**validated_data)
-        for client_address in client_address_data:
-            ClientAddress.objects.create(client=client_info, **client_address)
-        return client_info
+        address = validated_data.pop("client_address")
+        client = ClientInfo.objects.create(**validated_data)
+        ClientAddress.objects.create(**address)
+        return client
 
 
 class UserSerializer(serializers.ModelSerializer):
